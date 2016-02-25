4668f14cb6faee48ac3a6042594ff0ab5b8c5d76
simenrn <sironilssen@gmail.com>
2016-02-25 23:17:45 +0100

    Create oving6.go

---
+++ oving6.go
@@ -0,0 +1,24 @@
+package main
+
+import(
+	"fmt"
+	"net"
+	"os/exec"
+	"strings"
+)
+
+var baddr string = "129.241.187.255"
+var port string = "20021"
+
+
+/*
+func spawnProcess(){
+	cmd := exec.Command("gnome-terminal", "-x", "go", "run", "backup.go")
+}
+*/
+
+func 
+
+func main(){
+	
+}
