869e7aed89c4e8e633c21cd38f96337844a020fe
simenrn <sironilssen@gmail.com>
2016-04-13 07:09:34 +0200

    Create json.go

---
+++ Prosjekt/src/json/json.go
@@ -0,0 +1,24 @@
+package json
+
+import (
+	. "def"
+	"encoding/json"
+	"fmt"
+)
+
+func EncodeMsg(msg MSG) []byte {
+	encMsg, err := json.Marshal(msg)
+	if err != nil {
+		fmt.Println("Error encoding msg: ", err)
+	}
+	return encMsg
+}
+
+func DecodeMsg(msg []byte, lenght int) MSG {
+	var msg_rec MSG
+	err := json.Unmarshal(msg[:lenght], &msg_rec)
+	if err != nil {
+		fmt.Println("Error decoding msg: ", err)
+	}
+	return msg_rec
+}
