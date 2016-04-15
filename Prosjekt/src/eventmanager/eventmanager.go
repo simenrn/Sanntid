20f5b8ee7f2a8827847c4d2089c6a41ecfffca64
simenrn <sironilssen@gmail.com>
2016-04-15 14:11:29 +0200

    Added files via upload

--- Prosjekt/src/eventmanager/eventmanager.go
+++ Prosjekt/src/eventmanager/eventmanager.go
@@ -1,34 +1,34 @@
 package eventmanager
 
-import(
-	"fmt"
+import (
+	. ".././definitions"
+	. ".././elev"
 	. ".././json"
 	. ".././udp"
-	. ".././elev"
-	. ".././definitions"
+	"fmt"
 )
 
-func EventManager(send_ch, receive_ch chan Udp_message, orderEventChannel chan int){
+func EventManager(send_ch, receive_ch chan Udp_message, resendLostOrders, orderEventChannel chan int) {
 	go GetOrders(orderEventChannel)
 	go ElevLights()
-	
-
 
 	for {
 		select {
-			case UDP_Recv := <- receive_ch:
-				if UDP_Recv.Raddr != Laddr.String() {
-					fmt.Println("Laddr: ", Laddr.String())
-					fmt.Println("Received from: ", UDP_Recv.Raddr)
-					Dec_Msg := DecodeMsg(UDP_Recv.Data, UDP_Recv.Length)
-					UpdateOrder(Dec_Msg)
-					fmt.Println(Dec_Msg)
-				}
-			case <- orderEventChannel:
-				Udp_Msg.Data = EncodeMsg(Msg)
-				send_ch <- Udp_Msg
-				Msg.MessageType = NOTHING
-
+		case UDP_Recv := <-receive_ch:
+			if UDP_Recv.Raddr != Laddr.String() {
+				fmt.Println("Laddr: ", Laddr.String())
+				fmt.Println("Received from: ", UDP_Recv.Raddr)
+				Dec_Msg := DecodeMsg(UDP_Recv.Data, UDP_Recv.Length)
+				UpdateOrder(Dec_Msg, resendLostOrders)
+				fmt.Println(Dec_Msg)
+			}
+		case <-orderEventChannel:
+			Udp_Msg.Data = EncodeMsg(ElevatorList[0])
+			send_ch <- Udp_Msg
+			ElevatorList[0].FirstMsg = false
+			ElevatorList[0].MessageType = NOTHING
+		case <-resendLostOrders:
+			send_ch <- Udp_Msg
 		}
 	}
 }
