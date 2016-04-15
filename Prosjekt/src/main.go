4f572fa3fa7edab526aae188b1043aaa5dd48c24
simenrn <sironilssen@gmail.com>
2016-04-15 14:13:21 +0200

    Added files via upload

--- Prosjekt/src/main.go
+++ Prosjekt/src/main.go
@@ -1,22 +1,21 @@
 package main
 
 import (
+	. "./definitions"
 	. "./elev"
+	. "./eventmanager"
 	. "./statemachine"
 	. "./udp"
-	. "./definitions"
 	"fmt"
-	. "./eventmanager"
 	//"time"
 )
-
-
 
 func main() {
 
 	send_ch := make(chan Udp_message)
 	receive_ch := make(chan Udp_message)
 	OrderEventChannel := make(chan int)
+	ResendLostOrders := make(chan int)
 
 	go Udp_init(LOCAL_LISTEN_PORT, BROADCAST_LISTEN_PORT, MESSAGE_SIZE, send_ch, receive_ch)
 
@@ -26,25 +25,40 @@ func main() {
 
 	fmt.Println("Press STOP button to stop elevator and exit program\n")
 	//ElevSetMotorDirection(1)
-	
+	Msg.ReadyToGoUp = ON
+	OtherLift_1.MyIP = nil
+	OtherLift_2.MyIP = nil
+	Msg.FirstMsg = true
+
+	ElevatorList = append(ElevatorList, Msg)
+
 	go StateMachine(OrderEventChannel)
-	go EventManager(send_ch, receive_ch, OrderEventChannel)
-
-	
-
+	go EventManager(send_ch, receive_ch, ResendLostOrders, OrderEventChannel)
+	tempIPSUGMEG := ElevatorList[0].MyIP[len(ElevatorList[0].MyIP)-4 : len(ElevatorList[0].MyIP)]
+	tempIPSTRINGTRUSE := string(tempIPSUGMEG)
+	if tempIPSTRINGTRUSE == "129.241.187.153" {
+		fmt.Println("Du er HOMO")
+	}
+	fmt.Println("Byte til string kanskje?: ", tempIPSTRINGTRUSE)
+	fmt.Println("My IP: ", ElevatorList[0].MyIP[len(ElevatorList[0].MyIP)-4:len(ElevatorList[0].MyIP)])
+	fmt.Println("Lengden på IP: ", len(ElevatorList[0].MyIP))
+	fmt.Println("Siste byte av IP: ", ElevatorList[0].MyIP[len(ElevatorList[0].MyIP)-1])
 
 	for {
+
+		//fmt.Println("Other lift 1 IP: ", OtherLift_1.MyIP)
+		//fmt.Println("Other lift 2 IP: ", OtherLift_2.MyIP)
 		//fmt.Println("Local Address: ", Laddr)
 		//fmt.Println("Broadcast Address: ", Baddr)
-/*
-		fmt.Println("Interne ordre: ", Msg.Que_Local)
-		fmt.Println("Opp ordre: ", Msg.Que_Global_Up)
-		fmt.Println("Ned ordre: ", Msg.Que_Global_Down)
-		fmt.Println("Msg.PrevFloor: ", Msg.PrevFloor)
-		fmt.Println("Msg.Dirn: ", Msg.Dirn)
-		fmt.Println("Msg.State", Msg.State)
-		time.Sleep(time.Second * 3)
-*/
+		/*
+			fmt.Println("Interne ordre: ", ElevatorList[0].Que_Local)
+			fmt.Println("Opp ordre: ", ElevatorList[0].Que_Global_Up)
+			fmt.Println("Ned ordre: ", ElevatorList[0].Que_Global_Down)
+			fmt.Println("ElevatorList[0].PrevFloor: ", ElevatorList[0].PrevFloor)
+			fmt.Println("ElevatorList[0].Dirn: ", ElevatorList[0].Dirn)
+			fmt.Println("ElevatorList[0].State", ElevatorList[0].State)
+			time.Sleep(time.Second * 3)
+		*/
 		if ElevGetStopSignal() == 1 {
 			ElevSetMotorDirection(0)
 		}
